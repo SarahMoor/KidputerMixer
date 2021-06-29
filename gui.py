ec7a0c6c7daf22ba3ef6c7212a80a4cbbd079d25
SarahMoor <sarahmoor94@gmail.com>
2021-06-29 12:32:15 -0500

    Updating gui to have text entry boxes that can have time entered

--- gui.py
+++ gui.py
@@ -14,14 +14,20 @@ from tkinter import colorchooser
 from tkinter import Tk, Label, Button
 
 # importing motor control python
-from MotorControl import motor1, motor2, motor3, motor4
+#from MotorControl import motor1, motor2, motor3, motor4
 
 class ColorPicker:
     def __init__(self, master):
         self.master = master
         master.title('ColorPicker')
         
-        master.geometry('300x300')
+        master.geometry('500x500')
+        
+        self.time_var1 = IntVar()
+        self.time_var2 = IntVar()
+        self.time_var3 = IntVar()
+        self.time_var4 = IntVar()
+        
         
         self.label = Label(master, text="Please pick a color")
         self.label.pack()
@@ -31,12 +37,50 @@ class ColorPicker:
         self.color_button = Button(master, text="Color", command = self.ColorSpectrum)
         self.color_button.pack()
         
-        self.kill_button = Button(master, text="kill me", command = master.destroy)
+        self.motor1label = Label(master, text="Motor 1")
+        self.motor1entry = Entry(master, textvariable = self.time_var1)
+        
+        self.motor1label.pack()
+        self.motor1entry.pack()
+        
+        self.motor2label = Label(master, text="Motor 2")
+        self.motor2entry = Entry(master, textvariable = self.time_var2)
+        
+        self.motor2label.pack()
+        self.motor2entry.pack()
+        
+        self.motor3label = Label(master, text="Motor 3")
+        self.motor3entry = Entry(master, textvariable = self.time_var3)
+        
+        self.motor3label.pack()
+        self.motor3entry.pack()
+        
+        self.motor4label = Label(master, text="Motor 4")
+        self.motor4entry = Entry(master, textvariable = self.time_var4)
+        self.sub_btn=tk.Button(master, text = 'Submit', command = self.GetMotorSecs)
+    
+        
+        self.motor4label.pack()
+        self.motor4entry.pack()
+        self.sub_btn.pack()
+        
+        self.kill_button = Button(master, text="End", command = master.destroy)
         self.kill_button.pack()
         
         
+    def GetMotorSecs(self):
+        time1 = self.time_var1.get()
+        time2 = self.time_var2.get()
+        time3 = self.time_var3.get()
+        time4 = self.time_var4.get()
+
+        
+        print (time1)
+        print (time2)
+        print (time3)
+        print (time4)
+        
     def ColorSpectrum(self):
-     
         # variable to store hexadecimal code of color
         color_code = colorchooser.askcolor(title ="Choose color")
         print (color_code)
@@ -45,7 +89,8 @@ class ColorPicker:
         
     def PrintColor(self):
         print (self.color_chosen)
-        
+        print (self.time_var1)
+        print (self.time_var2)
         
         
     def RunMotor1(self, sec):
